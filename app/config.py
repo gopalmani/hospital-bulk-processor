c0399fead0f5b226f666198ae73ec16e2ca9536e
Deepak <deepakdubeygi@gmail.com>
2026-04-24 21:40:30 +0530

    Initial Commit

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://hospital-directory.onrender.com"
+BASE_URL = "https://hospital-bulk-processor-qkd0.onrender.com"
 MAX_HOSPITALS = 20
 CONCURRENT_REQUESTS = 5
 REQUEST_TIMEOUT = 20
